3f1a570705648612236387e98d79e449f5029d6d
SARTHAK <77231223+sarthak2807@users.noreply.github.com>
2023-06-26 16:42:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ import authRoutes from  "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js"
 import productRoutes from "./routes/productRoutes.js"
 import cors from 'cors';
+import path from 'path'
+import {fileURLToPath} from 'url';
 
 dotenv.config();
 
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express()
@@ -19,6 +25,7 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'./client/build')) )
 
 //routes
 app.use('/api/v1/auth', authRoutes);
@@ -26,8 +33,8 @@ app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product',productRoutes);
 
 //rest api
-app.get('/',(req,res) => {
-    res.send('<h1>Welcome to e shoppy app</h1>');
+app.use('*',function(req,res){
+   res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
 
 const PORT = process.env.PORT || 8080;
